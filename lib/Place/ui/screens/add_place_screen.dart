eb9490e74cfb29ab93403fd913863cdf9658b5ab
fab <fabri_kamus@hotmail.com>
2021-03-15 13:43:03 -0300

    Container para el fondo de add_place_screen con los datos nuevos

--- lib/Place/ui/screens/add_place_screen.dart
+++ lib/Place/ui/screens/add_place_screen.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app_3/widgets/gradient_back.dart';
+import 'package:flutter_app_3/widgets/text_input.dart';
 import 'package:flutter_app_3/widgets/title_header.dart';
 
 class AddPlaceScreen extends StatefulWidget{
@@ -23,11 +24,15 @@ class _AddPlaceScreen extends  State<AddPlaceScreen>{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+
+    final _controlerTitlePlace = TextEditingController();
+    final _controlerDescriptionPlace = TextEditingController();
+
     return Scaffold(
       body: Stack(
         children: <Widget>[
           GradientBack(height: 300.0),
-          Row(
+          Row(//App Bar
             children: <Widget>[
               Container(
                 padding: EdgeInsets.only(top: 25.0,left: 5.0),
@@ -47,7 +52,30 @@ class _AddPlaceScreen extends  State<AddPlaceScreen>{
                     padding: EdgeInsets.only(top: 45.0,left: 20.0,right: 20.0),
                     child: TitleHeader(title: "Add a new Place")
                   ))
+
             ],
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 120.9, bottom: 20.0),
+            child: ListView(
+              children:<Widget> [
+                Container(),//Foto
+                Container(//TextField Title
+                  margin: EdgeInsets.only(bottom: 20.0),
+                  child: TextInput(
+                    hintText: "Title",
+                    inputType: null,
+                    maxLines: 1,
+                    controller: _controlerTitlePlace,
+                  ),
+                ),
+                TextInput(
+                    hintText: "Description",
+                    inputType: TextInputType.multiline,
+                    maxLines: 4,
+                    controller: _controlerDescriptionPlace)
+              ],
+            ),
           )
         ],
       ),
