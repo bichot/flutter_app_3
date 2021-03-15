e69e6add378f88d248a713acf042b183fbc4248f
fab <fabri_kamus@hotmail.com>
2021-03-15 10:27:34 -0300

    Control desbordamiento texto

--- lib/User/ui/screens/sign_in_screen.dart
+++ lib/User/ui/screens/sign_in_screen.dart
@@ -17,11 +17,14 @@ class SignInScreen extends StatefulWidget {
 
 class _SignInScreen extends State<SignInScreen> {
   UserBloc userBloc;
+  double screenWidht;
+
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
 
+    screenWidht = MediaQuery.of(context).size.width;
     userBloc = BlocProvider.of(context);
     return _handleCurrentSession();
   }
@@ -48,19 +51,25 @@ class _SignInScreen extends State<SignInScreen> {
             Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Text(
-                  "Welcome \n This is your Travel App",
-                   style: TextStyle(
-                    fontSize: 37.0,
-                    fontFamily: "Lato",
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold),
-            ),
-            ButtonGreen(
-              text: "Login with Gmail",
-              onPressed: () {
-                userBloc.signOut();
-                userBloc.signIn().then((User user) {
+                Flexible(
+                  child: Container(
+                    width: screenWidht,
+                    child: Text(
+                      "Welcome \n This is your Travel App",
+                      style: TextStyle(
+                          fontSize: 37.0,
+                          fontFamily: "Lato",
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+
+              ButtonGreen(
+                text: "Login with Gmail",
+                onPressed: () {
+                 userBloc.signOut();
+                  userBloc.signIn().then((User user) {
                   userBloc.updateUserData(Model.User(
                       uid: user.uid,
                       name: user.displayName,
